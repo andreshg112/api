d495b44a2eb4d7120fdf040ef1c6f9c0edd46f29
andreshg112 <andreshg112@gmail.com>
2016-03-24 12:40:05 -0500

    Subidos los archivos.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,63 @@
+class Api::UsersController < ApplicationController
+	protect_from_forgery :only => [:create, :update, :destroy]
+	
+	def index
+		@users = User.all
+		respond_to do |format|
+			format.json { render json: @users }
+			format.xml { render xml: @users }
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
+		respond_to do |format|
+			format.json { render json: @user }
+			format.xml { render xml: @user }
+		end
+	end
+
+	def create
+		@user = User.new(user_params)
+		respond_to do |format|
+			if @user.save
+				format.json { render json: @user, status: :created }
+				format.xml { render xml: @user, status: :created }
+			else
+				format.json { render json: @user.errors, status: :unprocessable_entity }
+				format.xml { render xml: @user.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+	def update
+		@user = User.find(params[:id])
+		respond_to do |format|
+			if @user.update(user_params)
+				format.json { head :no_content, status: :ok }
+				format.xml { head :no_content, status: :ok }
+			else
+				format.json { render json: @user.errors, status: :unprocessable_entity }
+				format.xml { render xml: @user.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		respond_to do |format|
+			if @user.destroy
+				format.json { head :no_content, status: :ok }
+				format.xml { head :no_content, status: :ok }
+			else
+				format.json { render json: @user.errors, status: :unprocessable_entity }
+				format.xml { render xml: @user.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :email)
+	end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+
+	validates :email, :first_name, :last_name, :presence =>true
+	validates_uniqueness_of :email
+
+end
